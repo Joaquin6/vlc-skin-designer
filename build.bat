bca6b7bb0e1e6c57e1d85c32a51d824f5e42861b
Daniel Dreibrodt <daniel.dreibrodt@googlemail.com>
2007-09-07 10:34:32 +0000

    build script fix (icons were not included properly)

--- build.bat
+++ build.bat
@@ -3,7 +3,8 @@ cd src
 javac -d ../build vlcskineditor/Main.java
 cd ..
 copy src\vlcskineditor\FreeSans.ttf build\vlcskineditor\FreeSans.ttf
-copy src\vlcskineditor\icons build\vlcskineditor\icons
+md build\vlcskineditor\icons
+copy src\vlcskineditor\icons build\vlcskineditor\icons\
 echo Main-Class: vlcskineditor.Main>manifest_no_nb
 jar cvfm VLCSkinEditor.jar manifest_no_nb -C build .
 del manifest_no_nb
