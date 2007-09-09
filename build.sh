235703aae81576e9abe2a6239dd5153a0461cc7a
Antoine Cellerier <dionoea@videolan.org>
2007-09-09 17:35:27 +0000

    Create "VLCSkinEditor" launch script in the linux build script.

--- build.sh
+++ build.sh
@@ -9,3 +9,5 @@ cp src/vlcskineditor/icons/* build/vlcskineditor/icons
 echo "Main-Class: vlcskineditor.Main" > manifest_no_nb
 jar cvfm VLCSkinEditor.jar manifest_no_nb -C build .
 rm -f manifest_no_nb
+echo -e "#!/bin/sh\njava -jar VLCSkinEditor.jar\n" > VLCSkinEditor
+chmod u+x VLCSkinEditor
